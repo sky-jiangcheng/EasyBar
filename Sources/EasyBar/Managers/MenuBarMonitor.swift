c49a386df1b1f72978396bfd88c136f65721f853
jiangcheng <jiangcheng@cbib.com>
2026-09-03 14:33:03 +0800

    fix: use launchApplication for Status Bar apps (more reliable than open URL)

--- Sources/EasyBar/Managers/MenuBarMonitor.swift
+++ Sources/EasyBar/Managers/MenuBarMonitor.swift
@@ -178,9 +178,12 @@ final class MenuBarMonitor {
 
     func activateApp(_ item: MenuBarItem) {
         if item.appType == .statusbarOnly {
-            if let url = NSWorkspace.shared.urlForApplication(withBundleIdentifier: item.bundleIdentifier) {
-                NSWorkspace.shared.open(url)
-            }
+            NSWorkspace.shared.launchApplication(
+                withBundleIdentifier: item.bundleIdentifier,
+                options: [],
+                additionalEventParamDescriptor: nil,
+                launchIdentifier: nil
+            )
         } else {
             guard let app = NSWorkspace.shared.runningApplications.first(where: { $0.bundleIdentifier == item.bundleIdentifier }) else { return }
             app.activate()
